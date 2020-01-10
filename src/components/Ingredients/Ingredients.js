68d75b4135b4dd59232b2a0cd13064ff0266002b
Carlos Alcantar <carlosalcantar1223@gmail.com>
2020-01-09 17:02:17 -0800

    Seccion 26 465

--- src/components/Ingredients/Ingredients.js
+++ src/components/Ingredients/Ingredients.js
@@ -1,17 +1,33 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
 
-import IngredientForm from './IngredientForm';
-import IngredientList from './IngredientList';
-import Search from './Search';
+import IngredientForm from "./IngredientForm";
+import IngredientList from "./IngredientList";
+import Search from "./Search";
 
 const Ingredients = () => {
   const [userIngredients, setUserIngredients] = useState([]);
 
+  useEffect(() => {
+    fetch("https://react-hooks-update-57c3b.firebaseio.com/ingredients.json")
+    .then(response => response.json())
+    .then(responseData => {
+      const loadedIngredients = [];
+      for (const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        });
+      }
+      setUserIngredients(loadedIngredients);
+    });
+  },[]);
+
   const addIngredientHandler = ingredient => {
-    fetch('https://react-hooks-update-57c3b.firebaseio.com/ingredients.json', {
-      method: 'POST',
+    fetch("https://react-hooks-update-57c3b.firebaseio.com/ingredients.json", {
+      method: "POST",
       body: JSON.stringify(ingredient),
-      headers: { 'Content-Type': 'application/json' }
+      headers: { "Content-Type": "application/json" }
     })
       .then(response => {
         return response.json();
